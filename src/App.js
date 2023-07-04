0aaa7b388e0d68ca18fade767d76506daf51add1
Deiva Magalhaes <deivamagalhaes@helpscout.com>
2023-07-04 12:40:04 -0700

    Add styled-components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import HelpScout from '@helpscout/javascript-sdk';
 import './App.css';
+import Text from './components/Text/Text';
 
 function App() {
   const [context, setContext] = useState({})
@@ -22,6 +23,7 @@ function App() {
   return (
     <div className="App">
       <p>App created using create-react-app</p>
+      <Text>Styled component</Text>
       {conversation && <p>Conversation ID: {conversation?.id}</p>}
       {!conversation && <p>Loading context...</p>}
     </div>
